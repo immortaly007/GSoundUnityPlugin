b549012b66473970cb52ebb1297af54d4c024db3
immortaly007 <immortaly007@gmail.com>
2014-11-21 13:39:13 +0100

    Added C# project to test PInvoke of GSound

---
+++ GSoundUnity/GSoundCSharpTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSoundCSharpTest
+{
+    class Program
+    {
+        [DllImport("GSoundUnity.dll", EntryPoint = "?playSound@@YAXXZ")]
+        public static extern void playSound();
+
+        static void Main(string[] args)
+        {
+            playSound();
+        }
+    }
+}
